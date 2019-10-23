5ae35e4ea29d7e983182e44074f3c3151391b4a9
Apostolis Bekiaris <apostolis.bekiaris@gmail.com>
2019-10-23 20:27:37 +0300

    VSIX: Minor updates

--- Xpand.Plugins/Xpand.VSIX/ModelEditor/METoolWindowControl.Designer.cs
+++ Xpand.Plugins/Xpand.VSIX/ModelEditor/METoolWindowControl.Designer.cs
@@ -25,21 +25,34 @@ namespace Xpand.VSIX.ModelEditor {
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
+            this.panelControl1 = new DevExpress.XtraEditors.PanelControl();
             this.gridControl1 = new DevExpress.XtraGrid.GridControl();
             this.gridView1 = new DevExpress.XtraGrid.Views.Grid.GridView();
             this.gridColumnModels = new DevExpress.XtraGrid.Columns.GridColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.panelControl1)).BeginInit();
+            this.panelControl1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.gridView1)).BeginInit();
             this.SuspendLayout();
             // 
+            // panelControl1
+            // 
+            this.panelControl1.Controls.Add(this.gridControl1);
+            this.panelControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelControl1.Location = new System.Drawing.Point(0, 0);
+            this.panelControl1.Name = "panelControl1";
+            this.panelControl1.Size = new System.Drawing.Size(706, 675);
+            this.panelControl1.TabIndex = 0;
+            // 
             // gridControl1
             // 
             this.gridControl1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.gridControl1.Location = new System.Drawing.Point(0, 0);
+            this.gridControl1.Location = new System.Drawing.Point(2, 2);
             this.gridControl1.MainView = this.gridView1;
             this.gridControl1.Name = "gridControl1";
-            this.gridControl1.Size = new System.Drawing.Size(706, 675);
-            this.gridControl1.TabIndex = 0;
+            this.gridControl1.Size = new System.Drawing.Size(702, 671);
+            this.gridControl1.TabIndex = 1;
+            this.gridControl1.UseDirectXPaint = DevExpress.Utils.DefaultBoolean.False;
             this.gridControl1.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
             this.gridView1});
             // 
@@ -63,22 +76,23 @@ namespace Xpand.VSIX.ModelEditor {
             // gridColumnModels
             // 
             this.gridColumnModels.Caption = "Models";
+            this.gridColumnModels.FieldName = "Name";
             this.gridColumnModels.MinWidth = 30;
             this.gridColumnModels.Name = "gridColumnModels";
+            this.gridColumnModels.OptionsFilter.AutoFilterCondition = DevExpress.XtraGrid.Columns.AutoFilterCondition.Contains;
             this.gridColumnModels.Visible = true;
-            gridColumnModels.OptionsFilter.AutoFilterCondition = AutoFilterCondition.Contains;
             this.gridColumnModels.VisibleIndex = 0;
             this.gridColumnModels.Width = 112;
-            this.gridColumnModels.FieldName = "Name";
-
             // 
             // METoolWindowControl
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.gridControl1);
+            this.Controls.Add(this.panelControl1);
             this.Name = "METoolWindowControl";
             this.Size = new System.Drawing.Size(706, 675);
+            ((System.ComponentModel.ISupportInitialize)(this.panelControl1)).EndInit();
+            this.panelControl1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.gridView1)).EndInit();
             this.ResumeLayout(false);
@@ -87,8 +101,9 @@ namespace Xpand.VSIX.ModelEditor {
 
         #endregion
 
+        private DevExpress.XtraEditors.PanelControl panelControl1;
         private DevExpress.XtraGrid.GridControl gridControl1;
         private DevExpress.XtraGrid.Views.Grid.GridView gridView1;
-        private DevExpress.XtraGrid.Columns.GridColumn gridColumnModels;
+        private GridColumn gridColumnModels;
     }
 }
